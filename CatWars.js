67b22f9ba2ef7e7ddf9f6317ce1f25152d9d64e8
Brian Finck <brian@brianfinck.com>
2015-04-14 14:48:19 -0700

    random_image

--- CatWars.js
+++ CatWars.js
@@ -1,64 +1,67 @@
 'use strict';
 
-//$(document).ready(function() {
-  var catOjcArr = [];
-  var theCat1
-  var theCat2
+//(function($) {
+  var theCat1;
+  var theCat2;
+
   var catOjc = function(photoSource, name) {
     this.photoSource = photoSource;
     this.name = name;
+    this.vote = 0;
   };
 
-  var trackerOjc = function(vote, ect) {
-   this.vote = vote;
-  };
+  // var Tracker = function(vote, ect) {
+
+  // };
 
   //-----------trackerOjcfunctions---------
 
-  trackerOjc.prototype.vote = function() {
+  // trackerOjc.prototype.vote = function() {
 
-  };
+  // };
 
   //-----------catOjcfunctions--------------
 
-  catOjc.prototype.randomCat = function() {
+  catOjc.randomCat = function() {
    var randomNum1 = Math.ceil(Math.random() * 14) - 1;
-     console.log(randomNum1);
    var randomNum2 = Math.ceil(Math.random() * 14) - 1;
-     console.log(randomNum2);
-   var theCat1 = catOjcArr[randomNum1];
-     console.log(theCat1);
-   var theCat2 = catOjcArr[randomNum2];
-     console.log(theCat2);
+   theCat1 = catOjcArr[randomNum1];
+   theCat2 = catOjcArr[randomNum2];
       if (theCat1 === theCat2) {
-        catOjc.prototype.randomCat();
+        catOjc.randomCat();
       };
-
   };
 
-  catOjc.prototype.renderRandomCat = function() {
-    console.log(theCat1);
-    console.log(theCat2);
 
-  };
+  catOjc.renderRandomCat = function() {
+    catOjc.randomCat();
+    $('#image1').attr({
+      src: theCat1.photoSource
+    });
+    $('#image2').attr({
+      src: theCat2.photoSource
+    });
+
+    };
 
   //-----------catOjcList------------------
-  var cat1 = new catOjc ('catPhotos/01 - pPA0iM0.jpg', 'Bob');
-  var cat2 = new catOjc ('catPhotos/02 - 7ykJ1xi.jpg', 'Bill');
-  var cat3 = new catOjc ('catPhotos/03 - n91EiPd.jpg', 'Kylle');
-  var cat4 = new catOjc ('catPhotos/04 - LDiTFeQ.jpg', 'Joe');
-  var cat5 = new catOjc ('catPhotos/05 - ywavhKp.jpg', 'Rob');
-  var cat6 = new catOjc ('catPhotos/06 - 9Fg6CZS.jpg', 'Carll');
-  var cat7 = new catOjc ('catPhotos/07 - leaDJOY.jpg', 'Boo');
-  var cat8 = new catOjc ('catPhotos/08 - XA0PRs2.jpg', 'Dave');
-  var cat9 = new catOjc ('catPhotos/09 - rkO8XVX.jpg', 'Pat');
-  var cat10 = new catOjc ('catPhotos/10 - QBZjjMe.jpg', 'Rick');
-  var cat11 = new catOjc ('catPhotos/11 - aGJW6lS.jpg', 'Jason');
-  var cat12 = new catOjc ('catPhotos/12 - 60maZWt.jpg', 'Josh');
-  var cat13 = new catOjc ('catPhotos/13 - u9wzm0f.jpg', 'Criss');
-  var cat14 = new catOjc ('catPhotos/14 - vyz8MGP.jpg', 'Sack');
+  var catOjcArr = [
+    new catOjc('catPhotos/01 - pPA0iM0.jpg', 'Bob'),
+    new catOjc('catPhotos/02 - 7ykJ1xi.jpg', 'Bill'),
+    new catOjc('catPhotos/03 - n91EiPd.jpg', 'Kylle'),
+    new catOjc('catPhotos/04 - LDiTFeQ.jpg', 'Joe'),
+    new catOjc('catPhotos/05 - ywavhKp.jpg', 'Rob'),
+    new catOjc('catPhotos/06 - 9Fg6CZS.jpg', 'Carll'),
+    new catOjc('catPhotos/07 - leaDJOY.jpg', 'Boo'),
+    new catOjc('catPhotos/08 - XA0PRs2.jpg', 'Dave'),
+    new catOjc('catPhotos/09 - rkO8XVX.jpg', 'Pat'),
+    new catOjc('catPhotos/10 - QBZjjMe.jpg', 'Rick'),
+    new catOjc('catPhotos/11 - aGJW6lS.jpg', 'Jason'),
+    new catOjc('catPhotos/12 - 60maZWt.jpg', 'Josh'),
+    new catOjc('catPhotos/13 - u9wzm0f.jpg', 'Criss'),
+    new catOjc('catPhotos/14 - vyz8MGP.jpg', 'Sack')
+  ];
 
-catOjcArr.push(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12, cat13, cat14);
+  catOjc.renderRandomCat();
 
-
-//});
+//})(jQuery);
