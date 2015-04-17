27a7464d9940fb763f86d1adf04a0ffbc5cc61dd
Brian Finck <brian@brianfinck.com>
2015-04-17 14:55:30 -0700

    ready to got to server

--- index.js
+++ index.js
@@ -3,16 +3,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
 
-// app.get('/greet', function(request, response) {
-//   response.status(200).send('<h1>Hello Stranger!</h1>');
-// });
-
-// app.get('/greet/:person', function(req, res) {
-//   res.send('<h1>Hello ' + req.params.person + '</h1>');
-// });
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/*', function(req, res) {
   res.status(404).send('could not find page');
